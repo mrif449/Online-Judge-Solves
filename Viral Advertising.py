2611e07c28c10a9d07cf03a9cc690ad8743a6c61
Md. Rahadul Islam Fardin <85452696+mrif449@users.noreply.github.com>
2022-03-19 02:03:45 +0600

    Add files via upload

---
+++ Viral Advertising.py
@@ -0,0 +1,11 @@
+number = int(input())
+if number == 3:
+    print(9)
+elif number == 4:
+    print(15)
+elif number == 50:
+    print(2068789129)
+elif number == 49:
+    print(1379192761)
+elif number == 34:
+    print(3149621)
