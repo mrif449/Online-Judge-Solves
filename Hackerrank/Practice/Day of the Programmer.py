2d653cd67faea4f9f583a560ac903208b3e019bc
Md. Rahadul Islam Fardin <85452696+mrif449@users.noreply.github.com>
2022-04-09 00:24:39 +0600

    Add files via upload

--- Hackerrank/Practice/Day of the Programmer.py
+++ Hackerrank/Practice/Day of the Programmer.py
@@ -6,11 +6,10 @@ if year == 2400:
 elif year == 1918:
     print("26.09.1918")
 elif  year > 2000 and (year%100 == 0):
-        print(f"{not_leap}{year}")
+    print(f"{not_leap}{year}")
 elif  year <= 2000 and (year%100 == 0):
-        print(f"{leap}{year}")
+    print(f"{leap}{year}")
 elif (year%100 != 0) and (year%4 == 0):
     print(f"{leap}{year}")
-
 else:
     print(f"{not_leap}{year}")
